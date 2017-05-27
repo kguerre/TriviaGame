3381c086e2e785b27d9943fd4019dc9fd33648b3
kobe <kobe@vps-MacBook-Air.local>
2017-05-26 22:21:35 -0700

    updated trivia game files; added css and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 //VARIABLES
-var number = 30;
 var timeLeft;
+var number = 90;
 var scoreCorrect = 0;
 var scoreWrong = 0;
+// var totalScoreRight = 0;
+// var totalScoreWrong = 0;
 var guesses = [];
 var answers = [
 "Dwayne Wade",
@@ -24,6 +26,7 @@ var answers = [
 	
 //startGame()
 	function startGame() {
+		$("#questions").show();
 		startTimer();
 
 	}
@@ -46,11 +49,41 @@ var answers = [
 //stopTimer()
 	function stopTimer() {
 		clearInterval(timeLeft);
+		$("#questions").hide();
+		$("#score-board").show();
+		$('input:checked').each(function(){
+    	guesses.push($(this).val());
+	});
+		console.log(guesses)
 		scoreGame();
-	}
+	};
 //scoreGame()
 	function scoreGame() {
 		
+		var correctGuess = false
+		for (var i = 0; i < answers.length; i++) {
+			if(guesses[i] === answers[i]) {
+				correctGuess = true;
+				scoreCorrect++;
+				$("#right").html("Right Answers: " + scoreCorrect);
+				//console.log(scoreCorrect);
+			}else if(guesses[i] !== answers[i]) {
+				scoreWrong++;
+				$("#wrong").html("Wrong Answers: " + scoreWrong);
+			}
+		}
+		endGame();
+	};
+//endGame()
+	function endGame() {
+		if(scoreCorrect > 5) {
+			$("#win-loss").html("Well Done!");
+			$("#gif-goes-here").html('<img src="https://media.giphy.com/media/3o8dp2x07W2WEWwmY0/giphy.gif"/>');
+		}else {
+			$("#win-loss").html("Keep trying!");
+			$("#gif-goes-here").html('<img src="https://media.giphy.com/media/3o7qE8XB0J4nr53oRO/giphy.gif"/>');
+		}
 	}
+	
 
 });
