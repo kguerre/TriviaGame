004a7a7e32b5d5b814704ca26a50f8606d4369a4
Kathy Guerrero <krguerre@uci.edu>
2017-10-29 11:42:18 -0700

    remove comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,7 @@
-//VARIABLES
 var timeLeft;
 var number = 90;
 var scoreCorrect = 0;
 var scoreWrong = 0;
-// var totalScoreRight = 0;
-// var totalScoreWrong = 0;
 var guesses = [];
 var answers = [
 "Dwayne Wade",
@@ -18,57 +15,47 @@ var answers = [
 "24",
 "Michael Jordan"];
 
-//FUNCTIONS
-	$(document).ready(function() {
+$(document).ready(function() {
 
 	$("#start-button").on("click", startGame);
 	$("#submit").on("click", stopTimer);
 	
-//startGame()
-	function startGame() {
+    function startGame() {
 		$("#questions").show();
 		number = 90;
 		guesses= [];
 		startTimer();
-
 	}
-//startTimer()
-	function startTimer() {
+    function startTimer() {
 		timeLeft = setInterval(countDown, 1000);
 	}
-//counDown()
-	function countDown () {
+    function countDown () {
 		number--;
 		$("#seconds").html("Countdown: " + "<h2>" + number + "</h2>");
 		if (number === 0) {
 			gameOver()
 		}
 	}
-//gameOver()
-	function gameOver() {
+    function gameOver() {
 		stopTimer();
 	}
-//stopTimer()
-	function stopTimer() {
+    function stopTimer() {
 		clearInterval(timeLeft);
 		$("#questions").hide();
 		$("#score-board").show();
 		$('input:checked').each(function(){
-    	guesses.push($(this).val());
+    		guesses.push($(this).val());
 	});
 		console.log(guesses)
 		scoreGame();
 	};
-//scoreGame()
-	function scoreGame() {
-		
+    function scoreGame() {
 		var correctGuess = false
 		for (var i = 0; i < answers.length; i++) {
 			if(guesses[i] === answers[i]) {
 				correctGuess = true;
 				scoreCorrect++;
 				$("#right").html("Right Answers: " + scoreCorrect);
-				//console.log(scoreCorrect);
 			}else if(guesses[i] !== answers[i]) {
 				scoreWrong++;
 				$("#wrong").html("Wrong Answers: " + scoreWrong);
@@ -76,8 +63,7 @@ var answers = [
 		}
 		endGame();
 	};
-//endGame()
-	function endGame() {
+    function endGame() {
 		if(scoreCorrect > 5) {
 			$("#win-loss").html("Well Done!");
 			$("#gif-goes-here").html('<img src="https://media.giphy.com/media/3o8dp2x07W2WEWwmY0/giphy.gif"/>');
